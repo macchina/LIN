50937c6cbbfeb6109aff04de5e14e9c553360f39
Adam Voss <vossad01@gmail.com>
2017-08-24 10:37:02 -0500

    Release examples into the public domain using CC0

--- examples/Sniffer/Sniffer.ino
+++ examples/Sniffer/Sniffer.ino
@@ -1,33 +1,16 @@
-/*  Copyright (c) 2016 Macchina
+/*  LIN Sniffer - Monitor traffic on LIN Bus
  *
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the
- *  "Software"), to deal in the Software without restriction, including
- *  without limitation the rights to use, copy, modify, merge, publish,
- *  distribute, sublicense, and/or sell copies of the Software, and to
- *  permit persons to whom the Software is furnished to do so, subject to
- *  the following conditions:
+ *  Written in September 2016 by Blaž Pongrac B.S., RoboSap, Institute of Technology, Ptuj (www.robosap-institut.eu) for Macchina LLC
  *
- *  The above copyright notice and this permission notice shall be included
- *  in all copies or substantial portions of the Software.
+ *  To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- *  LIN Sniffer: Monitor traffic on LIN Bus
+ *  You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
  *  
- *  Using LIN Stack v2.0 Library.
+ *  Using LIN Stack v2.0 Library (https://github.com/macchina/LIN)
  *  
- *  Author: Blaž Pongrac B.S., RoboSap, Institute of Technology, Ptuj (www.robosap-institut.eu)
- *  
- *  Arduino IDE 1.6.9
- *  RoboSap, Institute of Technology, September 2016
+ *  Developed against Arduino IDE 1.6.9
  */
+
 // Including LIN Stack library
 #include <lin_stack.h>
 
